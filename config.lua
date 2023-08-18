d4823ea7477eccdf6a6b75a7db704e6f8decf0b0
FlayingWolf <49660373+FlayingWolf@users.noreply.github.com>
2023-08-18 09:40:48 -0400

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,22 @@
+Config = {
+    locations = {--voting locations, default is with gabz townhall
+        vector3(-541.78, -182.48, 38.23),
+        vector3(-542.66, -181.12, 38.23),
+        vector3(-543.43, -179.86, 38.23),
+        vector3(-534.05, -174.5, 38.23),
+        vector3(-533.28, -175.82, 38.23),
+        vector3(-532.53, -177.13, 38.23),
+    },
+    webhook = "https://discord.com/api/webhooks/1136407823606104136/YH4b_tlFc7YGRIowDmS8aGJBXLsqF6TF-8CrDT0xEbYMZpdO0Rqr7lfh4wbALh4EceIb",--YOUR_WEBHOOK
+    thumbnail = "https://imgur.com/t2tyrHC.png",--YOUR_LOGO
+    name = {--voting locations, default is with gabz townhall
+        candidate1 = 'Candidate 1',
+        candidate2 = 'Candidate 2',
+        candidate3 = 'Candidate 3',
+    },
+    image = {--voting locations, default is with gabz townhall
+        candidate1 = 'https://i.imgur.com/4MCzeEZ.png',
+        candidate2 = 'https://i.imgur.com/4MCzeEZ.png',
+        candidate3 = 'https://i.imgur.com/4MCzeEZ.png',
+    },
+}
---
+++ fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'cerulean'
+games {'gta5'}
+
+author 'FlayingWolf'
+discord 'https://discord.gg/GynMb6kCrn'
+
+shared_scripts {
+	'@ox_lib/init.lua',
+	'config.lua'
+}
+
+client_scripts {
+	'client/main.lua'
+}
+
+server_scripts {
+	'server/main.lua'
+}
+
+lua54 'yes'
