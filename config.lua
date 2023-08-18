c35e591c6b579a73f4c1d79ad51363e2e03724e0
FlayingWolf <49660373+FlayingWolf@users.noreply.github.com>
2023-08-18 09:41:31 -0400

    Update config.lua

--- config.lua
+++ config.lua
@@ -7,8 +7,8 @@ Config = {
         vector3(-533.28, -175.82, 38.23),
         vector3(-532.53, -177.13, 38.23),
     },
-    webhook = "https://discord.com/api/webhooks/1136407823606104136/YH4b_tlFc7YGRIowDmS8aGJBXLsqF6TF-8CrDT0xEbYMZpdO0Rqr7lfh4wbALh4EceIb",--YOUR_WEBHOOK
-    thumbnail = "https://imgur.com/t2tyrHC.png",--YOUR_LOGO
+    webhook = "YOUR_WEBHOOK",
+    thumbnail = "YOUR_LOGO",
     name = {--voting locations, default is with gabz townhall
         candidate1 = 'Candidate 1',
         candidate2 = 'Candidate 2',
